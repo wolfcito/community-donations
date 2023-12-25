77dcf9baa90ca70b4baa1b69c5b276fa331d6070
Luis Fernando <guffenix@gmail.com>
2023-12-25 17:11:18 -0500

    feat: useful icons

---
+++ components/icons/index.ts
@@ -0,0 +1,2 @@
+export * from './loader.icon'
+export * from './view.icon'
---
+++ components/icons/view.icon.tsx
@@ -0,0 +1,10 @@
+export function ViewIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg width={800} height={800} viewBox="0 0 16 16" {...props}>
+      <path
+        fill="current"
+        d="M8 2A7.998 7.998 0 0 0 .262 8.008 8.002 8.002 0 0 0 8 14a7.997 7.997 0 0 0 7.738-6.012A8.003 8.003 0 0 0 8 2zm0 2c2.21 0 4 1.79 4 4s-1.79 4-4 4-4-1.79-4-4 1.79-4 4-4zm0 2a2 2 0 1 0 2 2c0-1.105-.895-2-2-2zm0 0"
+      />
+    </svg>
+  )
+}
